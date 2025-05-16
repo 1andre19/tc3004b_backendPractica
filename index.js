6f2119dd5f4f9f46678089bd61db1da86676c47e
andre <111261206+1andre19@users.noreply.github.com>
2025-05-15 20:21:34 -0600

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ const app = express();
 const port = 5000;
 
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'], 
+  credentials: true 
+}));
+
 app.use(indexRoutes);
 app.use(itemsRoutes);
 app.use(loginRoutes);
